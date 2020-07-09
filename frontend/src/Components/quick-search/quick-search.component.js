8f8b118185d68cd995f9ae00df5494a350809523
TianruiLiu <tliu1@macalester.edu>
2020-07-08 23:44:38 -0500

    quick search

---
+++ frontend/src/Components/quick-search/quick-search.component.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import './quick-search.styles.css';
+
+export const QuickSearch=({placeholder,handleChange})=>(<input 
+    className ='search'    
+    type='search' 
+    placeholder={placeholder} 
+    onChange={handleChange}/>)
