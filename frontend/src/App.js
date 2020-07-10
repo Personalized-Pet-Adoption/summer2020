8bfd3a1880b846e67d8767bb79896abcd185f345
luli2949 <13612124718@163.com>
2020-07-09 19:54:35 -0500

    component corrected

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { QuickSearch } from './Components/quick-search/quick-search.component';
@@ -24,7 +24,7 @@ import { QuickSearch } from './Components/quick-search/quick-search.component';
 //   );
 // }
 
-class App extends Components{
+class App extends Component{
   constructor(){
     super();
     this.state={
