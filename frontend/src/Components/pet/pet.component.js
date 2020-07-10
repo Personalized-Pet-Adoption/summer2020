01f6cfc2568f91a8444a8130b36eec5dc6063ce8
luli2949 <13612124718@163.com>
2020-07-09 19:50:59 -0500

    added pet and pet list

--- frontend/src/Components/pet/pet.component.js
+++ frontend/src/Components/pet/pet.component.js
@@ -1,3 +1,12 @@
 import React from 'react';
-import './pet.styples.css';
+import './pet.styles.css';
 
+export const Pet = props =>(
+    <div className = 'pet-container'>
+        <img alt = "pet"   />
+        <h2>
+            {props.Pet}
+        </h2>
+        <p>{props.Pet.name}</p> 
+    </div>
+);
