3c1f8c754de2353316cc481805687b3eeb912913
chenyyxx <chenyx.eric2013@gmail.com>
2020-07-13 23:59:20 -0500

    added the first API getAllPets()
    Able to return the data of first 50 pets in the database ordered by post date

--- backend/petAdoption/views.py
+++ backend/petAdoption/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import Pet
+from django.http import HttpResponse
+
+from django.core import serializers
 
 # Create your views here.
+
+# return the data of first 50 pets ordered by post date
+def get_all_pets(request):
+    pet_list = Pet.objects.order_by('-post_date')[:50]
+    output = serializers.serialize("json", pet_list)
+    return HttpResponse(output)
